c8b3bd3ce734b59373b484c22e3869e593053618
Kimber Ogden <mx.kimber@gmail.com>
2023-03-24 15:08:45 -0500

    first commit

---
+++ dictionary_app.rb
@@ -0,0 +1,26 @@
+require 'http'
+
+
+puts "Please enter a word"
+user_input = gets.chomp
+response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_input}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
+definitions = response.parse(:json)
+
+p definitions[1]['text']
+# ^^ assign to variable
+
+#pronunciation
+
+
+
+
+
+
+
+
+
+# Build a terminal dictionary app
+# Create a new ruby file called dictionary_app.rb
+# The program should ask the user to enter a word, then use the wordnik API to show the word’s definition: https://developer.wordnik.com/docs#!/word/getDefinitions
+# Once that works, have it also display the top example and pronunciation (browse through the documentation to the top example and pronunciation).
+# Bonus: Write your program in a loop such that the user can keep entering new words without having to restart the program. Give them the option of entering q to quit.
